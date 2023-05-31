86de07ed95211450550b13c766df2ca8813ab55a
Said Augusto <131319843+Sharkorim@users.noreply.github.com>
2023-05-31 19:15:01 -0400

    Create CARTE.mk

---
+++ CARTE.mk
@@ -0,0 +1,25 @@
+# Sistemas de Coordenadas
+
+- Coordenadas Polares
+  - Representación de puntos en el plano con distancia radial (r) y ángulo (θ).
+  - Utiliza un eje polar y un origen (O).
+  - Coordenadas denotadas como (r, θ).
+  - θ se mide en sentido antihorario desde el eje polar.
+
+- Coordenadas Cartesianas
+  - Representación de puntos en el espacio con tres ejes ortogonales (x, y, z).
+  - Utiliza un origen (O) y tres ejes perpendiculares (x, y, z).
+  - Coordenadas denotadas como (x, y, z).
+  - La posición se determina por las distancias desde el origen a lo largo de cada eje.
+
+- Coordenadas Cilíndricas
+  - Representación de puntos en el espacio con distancia radial (ρ), ángulo azimutal (φ) y altura (z).
+  - Utiliza un eje z, un eje radial y un eje azimutal.
+  - Coordenadas denotadas como (ρ, φ, z).
+  - φ se mide en sentido antihorario desde el eje azimutal en el plano xy.
+
+- Coordenadas Esféricas
+  - Representación de puntos en el espacio con distancia radial (r), ángulo azimutal (θ) y ángulo de elevación (φ).
+  - Utiliza un origen, un eje radial, un eje azimutal y un eje de elevación.
+  - Coordenadas denotadas como (r, θ, φ).
+  - θ se mide en sentido antihorario desde el eje azimutal en el plano xy, y φ representa la elevación desde el plano xy hacia arriba o hacia abajo.
